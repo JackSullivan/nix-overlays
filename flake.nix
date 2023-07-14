e1054c18abc7a4877e017b73f305e133d69b38f7
John Sullivan <john.t.sullivan@gmail.com>
2023-07-14 16:41:46 -0400

    added default template

--- flake.nix
+++ flake.nix
@@ -10,20 +10,20 @@
       overlay = final: prev:
         prev.lib.composeManyExtensions (prev.lib.attrValues overlaySet);
       overlays = overlaySet // { default = overlay; };
-      templates = {
+      templates = let
         basic = {
           path = ./templates/basic;
           description = "Basic flake for multisystem nixpkgs";
         };
-
+      in {
+        inherit basic;
+        default = basic;
         devShell = {
           path = ./templates/devShell;
           description = "Basic flake for multisystem devShells";
         };
       };
-      nixosModules = {
-        maven = import ./modules/maven;
-      };
+      nixosModules = { maven = import ./modules/maven; };
     in { inherit overlays templates nixosModules; };
 
 }

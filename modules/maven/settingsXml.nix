5008008cd2a9f4c2b3ebd9f961853c1fd052f17e
John Sullivan <john.t.sullivan@gmail.com>
2023-09-19 15:01:53 -0400

    adding support for profile properties

--- modules/maven/settingsXml.nix
+++ modules/maven/settingsXml.nix
@@ -1,13 +1,13 @@
 { lib }:
 
 cfg: let
-  inherit (lib.attrsets) mapAttrsToList;
+  inherit (lib.attrsets) mapAttrsToList optionalAttrs;
   inherit (lib.generators) toXMLFile;
   servers.server = mapAttrsToList (id: server: {inherit id;} // server) cfg.servers;
   proxies.proxy = mapAttrsToList (id: proxy: {inherit id;} // proxy) cfg.proxies;
   mkRepositories = repos: {repository = mapAttrsToList (id: repository: {inherit id;} // repository) repos;};
   # repositories.repository = mapAttrsToList (id: repository: {inherit id;} // repository) cfg.repositories;
-  profiles.profile = mapAttrsToList (id: {repositories, ...}: {inherit id; repositories = mkRepositories repositories;}) cfg.profiles;
+  profiles.profile = mapAttrsToList (id: args@{repositories, ...}: {inherit id; repositories = mkRepositories repositories;} // (optionalAttrs ()builtins.hasAttr "properties" args) args.properties) cfg.profiles;
   activeProfiles.activeProfile = cfg.activeProfiles;
 in toXMLFile {
   name = "settings.xml";

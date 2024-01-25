27e3c988604cc9a9084f990c58f04741549442ee
John Sullivan <john.t.sullivan@gmail.com>
2024-01-25 11:06:09 -0500

    added envrc to devshell template

--- templates/devShell/flake.nix
+++ templates/devShell/flake.nix
@@ -1,13 +1,11 @@
 {
   description = "A very basic flake";
 
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem(system:
-    let pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
-        buildInputs = [];
-      };
-    });
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in { devShells.default = pkgs.mkShell { buildInputs = [ ]; }; });
 
 }
